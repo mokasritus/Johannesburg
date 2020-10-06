cdb6c314b92591b47225c86aa0729e95c7720990
Rita Csullag <csullagrita@gmail.com>
2020-10-06 11:17:50 +0200

    c5

--- src/Valami.java
+++ src/Valami.java
@@ -1,11 +1,13 @@
 public class Valami {
 
   public static void main(String[] args) {
-    String parameter = "Szeged";
-    cheer(parameter);
+    String parameter1 = "Szeged";
+    String parameter2 = "boldogsag";
+    cheer(parameter1, parameter2);
   }
 
-  private static void cheer(String parameter) {
-    System.out.println("most jön a Java");
+  private static void cheer(String parameter1, String parameter2) {
+    System.out.println(parameter1);
+    System.out.println(parameter2);
   }
 }
