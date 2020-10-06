8ca968c5a190c3c559083cfc79cfd83463934917
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-10-06 11:16:44 +0200

    b5

--- src/GreetMethodA.java
+++ src/GreetMethodA.java
@@ -1,6 +1,8 @@
 public class GreetMethodA {
+  String name = "Anikó";
+  String greeting = "Hello";
 
-  public static void greet () {
-    System.out.println("Greetings");
+  public static void greet (String greeting, String name) {
+    System.out.println(greeting  + " " + name);
   }
 }
